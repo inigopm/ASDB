819f09f8bfb533f2833aff264714e2297611db73
Javier Herrer Torres <herrertj@amazon.com>
2023-10-27 18:52:10 +0200

    feat: memory profiling

--- operations.py
+++ operations.py
@@ -1,24 +1,28 @@
 import sys
 import runpy
+from memory_profiler import profile
 
 PATH = "./scripts/"
 PATH_ARTICLES = PATH + "articles/"
 PATH_LACIUDADALDIA = PATH + "laciudadaldia/"
 
+@profile
+def main():
+    print("Running ARTICLES scripts")
+    runpy.run_path(PATH_ARTICLES + 'landing_zone.py')
+    runpy.run_path(PATH_ARTICLES + 'formated_zone.py')
+    runpy.run_path(PATH_ARTICLES + 'trusted_zone.py')
 
-print("Running ARTICLES scripts")
-#TODO: Iñigo cambiar las rutas de absolutas a relativas
-runpy.run_path(PATH_ARTICLES + 'landing_zone.py')
-runpy.run_path(PATH_ARTICLES + 'formated_zone.py')
-runpy.run_path(PATH_ARTICLES + 'trusted_zone.py')
+    print("Running LACIUDADALDIA scripts")
+    runpy.run_path(PATH_LACIUDADALDIA +"landing_zone.py")
+    runpy.run_path(PATH_LACIUDADALDIA + "formatted_zone.py")
+    runpy.run_path(PATH_LACIUDADALDIA + "trusted_zone.py")
 
-print("Running LACIUDADALDIA scripts")
-runpy.run_path(PATH_LACIUDADALDIA +"landing_zone.py")
-runpy.run_path(PATH_LACIUDADALDIA + "formatted_zone.py")
-runpy.run_path(PATH_LACIUDADALDIA + "trusted_zone.py")
+    print("Running EXPLOITATION ZONE scripts")
+    runpy.run_path(PATH + "exploitation_zone/concept_1.py")
+    runpy.run_path(PATH + "exploitation_zone/concept_2.py")
 
-print("Running EXPLOITATION ZONE scripts")
-runpy.run_path(PATH + "exploitation_zone/concept_1.py")
-runpy.run_path(PATH + "exploitation_zone/concept_2.py")
+    print("The Data Pipeline run successfully")
+    
 
-print("The Data Pipeline run successfully")
+main()
